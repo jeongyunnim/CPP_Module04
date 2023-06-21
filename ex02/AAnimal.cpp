38d73334f4c48fe30091ca3db336bef6f7d9ee54
jeongyunnim <jeongyun7521@gmail.com>
2023-06-21 15:18:12 +0900

    ex02 가상클래스도 Orthodox canonical form 지킴

--- ex02/AAnimal.cpp
+++ ex02/AAnimal.cpp
@@ -11,6 +11,17 @@ AAnimal::~AAnimal(void)
 	std::cout << "destructor called. AAnimal Class deleted." << std::endl;
 }
 
+AAnimal& AAnimal::operator=(const AAnimal& rhs)
+{
+	_type = rhs._type;
+	return (*this);
+}
+
+AAnimal::AAnimal(const AAnimal& other)
+	:	_type(other._type)
+{
+	std::cout << "Copy constructor called. AAnimal Class Generated." << std::endl;
+}
 
 
 std::string		AAnimal::getType(void) const
--- ex02/AAnimal.hpp
+++ ex02/AAnimal.hpp
@@ -8,6 +8,8 @@ public:
 
 	AAnimal(void);
 	virtual ~AAnimal(void);
+	AAnimal& operator=(const AAnimal& rhs);
+	AAnimal(const AAnimal& other);
 
 	virtual void	makeSound(void) const = 0;
 	std::string		getType(void) const;
